4c4b71f764ca14d83c13dc5d597f857f4f3a5a03
SiKirillo <samarychev98@mail.ru>
2020-03-08 22:44:14 +0300

    v0.1
    components

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import nav from './Navbar.module.css';
+
+const Navbar = () => {
+    return <nav className={nav.appNav}>
+        <div className={nav.item}>
+            <a>Profile</a>
+        </div>
+        <div className={nav.item}>
+            <a>Messages</a>
+        </div>
+        <div className={nav.item}>
+            <a>News</a>
+        </div>
+        <div className={nav.item}>
+            <a>Music</a>
+        </div>
+        <div className={nav.item}>
+            <a>Settings</a>
+        </div>
+    </nav>
+};
+
+export default Navbar;
